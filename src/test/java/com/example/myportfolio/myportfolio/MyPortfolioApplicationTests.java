18ca5f55ed2263b2d2e7833b5621a310503c2d47
Erick Resurreccion <72366477+ResurreccionErick@users.noreply.github.com>
2022-11-03 01:03:10 +0800

    Initial commit

---
+++ src/test/java/com/example/myportfolio/myportfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myportfolio.myportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
